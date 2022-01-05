b28821628afd1445d8f4632ccc9eca7841c0ef19
Rajeshkannan <rajeshkannan.e@gmail.com>
2022-01-05 23:16:07 +0530

    Create printJava.java

---
+++ printJava.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+
+class Main {
+    public static void main(String[] args) {
+
+        String data = "This is a text inside the file.";
+
+        try {
+            PrintStream output = new PrintStream("output.txt");
+
+            output.print(data);
+            output.close();
+        }
+        catch(Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
